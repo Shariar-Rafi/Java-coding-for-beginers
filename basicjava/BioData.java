36d50d3a24582cf3b2f6724c94f1ff840c088b39
Shariar Rafi <112250933+Shariar-Rafi@users.noreply.github.com>
2022-08-28 00:37:44 +0600

    Add files via upload

---
+++ basicjava/BioData.java
@@ -0,0 +1,12 @@
+package basicjava;
+public class BioData 
+{
+    public static void main(String[] args)
+    {
+      
+               
+    System.out.println("Name: Shariar Rafi \tPhone: 0170xxxxxxxxx \nE-mail: shariar.rafi@gmail.com\nAdress: Dhaka,Bangladesh");
+
+
+    }
+}
